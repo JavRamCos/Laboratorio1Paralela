5b8385922ddc20e063d4d294dc1fb5ffb0d7a5a9
coutino568 <40869460+coutino568@users.noreply.github.com>
2023-02-22 13:32:24 -0600

    Schedule y blocksize

--- piSeriesNaive3.c
+++ piSeriesNaive3.c
@@ -5,15 +5,15 @@
 
 int main(int argc, char* argv[]) {
     int N = 10e8;
-    int thread_count = 1;
-
+    int thread_count = 6;
+    int block_size = 128;
     clock_t t;
     t = clock();
 
     double factor = 1.0;
     double sum, pi_approx = 0.0;
     
-    #pragma omp parallel for num_threads(thread_count) private( factor, sum) \
+    #pragma omp parallel for num_threads(thread_count) private( factor, sum) schedule (dynamic, block_size)\
     reduction(+:sum)
     for (int k = 0; k < N; k++) {
         factor = (k % 2 == 0) ? 1.0 : - 1.0;
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 
-    printf("Pi: %.25f | iterations %d |THREADS: %d  | TIME TAKEN : %f \n", pi_approx, N, thread_count, time_taken);
+    printf("Pi: %.25f | iterations %d |THREADS: %d  | TIME TAKEN : %f | BLOCK SIZE: %d  \n", pi_approx, N, thread_count, time_taken, block_size);
 
     return 0;
 }
